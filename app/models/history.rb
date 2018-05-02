3825bc20121825a40ff10d4e6f48e8c4ce5c7fd9
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-05-02 18:01:27 +0900

    [fix]バリデートの指定カラムを修正

--- app/models/history.rb
+++ app/models/history.rb
@@ -2,5 +2,5 @@ class History < ApplicationRecord
   belongs_to :book
   validates :book_id  , presence: true
   validates :user_id  , presence: true
-  validates :return_date  , presence: true
+  validates :checkout_date  , presence: true
 end
