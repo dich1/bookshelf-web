d0dcc2beb6733a60aef56fb39997c463a5aabd48
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-05-03 14:26:50 +0900

    [feat]本一覧取得APIを追加

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -3,16 +3,24 @@
 # 返却値がある場合、JSON形式で返す
 class Api::BooksController < ApplicationController
   before_action :set_book, only: [:update, :destroy]
+  # TODO ENUMにする。用途の範囲が広くなったら外に切り出す。
+  LENDING     = 0
+  SAFEKEEPING = 1
 
+  # 返却値をラップするための変数を生成
   def initialize
+    @ary  = Array.new
     @hash = Hash.new { |h, k| h[k] = [] }
   end
 
   # 本一覧取得API
   # GET /api/books
   def index
-    @api_books = Book.all
-    @hash["books"] = @api_books
+    @books = Book.all
+
+    set_books_response(@books)
+
+    @hash["books"] = @ary
     
     render :json => @hash
   end
@@ -59,5 +67,37 @@ class Api::BooksController < ApplicationController
     def book_params
       params.fetch(:book, {})
     end
+
+    # 貸出中かを判定する
+    # 
+    # @param  [Object]  book 本一覧の中の1冊
+    # @return [Boolean] 貸出中の場合         :true 
+    # @return [Boolean] それ以外の場合(保管中):false
+    def is_lending(book)
+      if book.histories.where.not(checkout_date: nil).where(return_date: nil).last.nil?
+        return false
+      end
+
+      return true
+    end
+
+    # 本の一覧として返す返却値を設定する
+    # 
+    # @param [Object] books Bookモデルから取得した本一覧
+    def set_books_response(books)
+      books.each {|book| 
+        # 1冊毎の状態と返却予定日を設定する
+        status = SAFEKEEPING
+        return_date = ''
+        if is_lending(book)
+          status = LENDING
+          return_date = book.histories.where.not(checkout_date: nil, return_date: nil).last.return_date
+        end
+        
+        hash_book = book.attributes
+        hash_book["status"] = status;
+        hash_book["return_date"] = return_date;
+        @ary << hash_book
+      }
     end
 end
