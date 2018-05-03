af83c607342bdc129d82a7876fb96a8314415cc3
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-05-03 09:51:00 +0900

    [fix]返却日を必須項目から削除

--- app/models/history.rb
+++ app/models/history.rb
@@ -2,5 +2,4 @@ class History < ApplicationRecord
   belongs_to :book, class_name: 'Book', foreign_key: 'book_id'
   validates :book_id  , presence: true
   validates :user_id  , presence: true
-  validates :checkout_date  , presence: true
 end
