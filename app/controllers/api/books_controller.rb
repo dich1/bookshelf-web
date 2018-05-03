735045d1342efa082d1c839f8fb98362ccd71202
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-05-03 13:32:52 +0900

    [refactor]変数名の修正

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -1,5 +1,5 @@
 class Api::BooksController < ApplicationController
-  before_action :set_api_book, only: [:update, :destroy]
+  before_action :set_book, only: [:update, :destroy]
 
   def initialize
     @hash = Hash.new { |h, k| h[k] = [] }
@@ -15,13 +15,13 @@ class Api::BooksController < ApplicationController
 
   # POST /api/books.json
   def create
-    @api_book = Book.new(api_book_params)
+    @book = Book.new(book_params)
 
     respond_to do |format|
-      if @api_book.save
-        format.json { render :show, status: :created, location: @api_book }
+      if @book.save
+        format.json { render :show, status: :created, location: @book }
       else
-        format.json { render json: @api_book.errors, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -29,17 +29,17 @@ class Api::BooksController < ApplicationController
   # PATCH/PUT /api/books/1.json
   def update
     respond_to do |format|
-      if @api_book.update(api_book_params)
-        format.json { render :show, status: :ok, location: @api_book }
+      if @book.update(book_params)
+        format.json { render :show, status: :ok, location: @book }
       else
-        format.json { render json: @api_book.errors, status: :unprocessable_entity }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /api/books/1.json
   def destroy
-    @api_book.destroy
+    @book.destroy
     respond_to do |format|
       format.json { head :no_content }
     end
@@ -47,12 +47,13 @@ class Api::BooksController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_api_book
-      @api_book = Book.find(params[:id])
+    def set_book
+      @book = Book.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def api_book_params
-      params.fetch(:api_book, {})
+    def book_params
+      params.fetch(:book, {})
+    end
     end
 end
