35f7fd7675ba492858a041daaf168c2efa1f6510
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-05-12 21:37:29 +0900

    [refactor]不要な記述を削除

--- app/controllers/api/histories_controller.rb
+++ app/controllers/api/histories_controller.rb
@@ -10,31 +10,26 @@ class Api::HistoriesController < ApplicationController
   def create
     @history = History.new(history_params)
 
-    respond_to do |format|
-      if @history.save
-        format.json { render :show, status: :created, location: @history }
-      else
-        format.json { render json: @history.errors, status: :unprocessable_entity }
-      end
+    if @history.save
+      render :show, status: :created
+    else
+      render json: @history.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /api/histories/1.json
   def update
-    respond_to do |format|
-      if @history.update(history_params)
-        format.json { render :show, status: :ok, location: @history }
-      else
-        format.json { render json: @history.errors, status: :unprocessable_entity }
-      end
+    if @history.update(history_params)
+      render :show, status: :ok, location: @history
+    else
+      render json: @history.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /api/histories/1.json
   def destroy
     @history.destroy
-    respond_to do |format|
-      format.json { head :no_content }
+      head :no_content
     end
   end
 
