ed92c6363989240388a257ec03b30b40dbc537cf
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-05-13 15:45:57 +0900

    [feat]本更新APIを追加

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -13,22 +13,25 @@ class Api::BooksController < ApplicationController
     render :json => @books
   end
 
+  # 本登録API。同時に借りる場合、historiesにユーザーIDと返却日を登録する
+  #
   # POST /api/books
   def create
     @book = Book.new(book_params)
 
     if @book.save
-      # historiesにユーザーIDと返却日を登録する
       render :json => {}, status: :created
     else
       render json: @book.errors, status: :unprocessable_entity 
     end
   end
 
+  # 本更新API。返却情報を更新する場合、historiesの返却日予定日または返却日を更新する
+  #
   # PATCH/PUT /api/books/:id
   def update
     if @book.update(book_params)
-      render :show, status: :ok, location: @book 
+      render :json => {}, status: :no_content
     else
       render json: @book.errors, status: :unprocessable_entity 
     end
@@ -48,6 +51,6 @@ class Api::BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.fetch(:book, {}).permit(:title, :image, :genre_id, histories_attributes: [:user_id, :checkout_date, :return_due_date])
+      params.fetch(:book, {}).permit(:id, :title, :image, :genre_id, histories_attributes: [:book_id, :user_id, :checkout_date, :return_due_date, :return_date, :_destroy])
     end
 end
