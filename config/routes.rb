ef865cf8b72debf1e02147449480d382fe4cbf0a
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-05-20 21:30:04 +0900

    [style]インデントを修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :books, only: [:index, :create, :update, :destroy]
-    resources :memos, only: [:index, :create, :update, :destroy]
+    resources :books    , only: [:index, :create, :update, :destroy]
+    resources :memos    , only: [:index, :create, :update, :destroy]
     resources :histories, only: [:index, :create, :update, :destroy]
+    resources :users    , only: [:show]
   end
 end
