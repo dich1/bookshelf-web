bd27b8f995a397be0f0402d27665291b8384f5cc
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-07-01 11:10:57 +0900

    [feat]webサーバの設定記述を追加

--- config/puma.rb
+++ config/puma.rb
@@ -54,3 +54,9 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+app_dir = File.expand_path("../..", __FILE__)
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
