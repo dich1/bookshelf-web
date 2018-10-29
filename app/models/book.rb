62f6d30aa405132550672493931642a6ee9b0697
大智 伊藤 <daich125912@gmail.com>
2018-10-29 13:42:25 +0000

    [chore]条件毎に本一覧を取得

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,11 +8,34 @@ class Book < ApplicationRecord
   mount_uploader :image, ImageUploader
   scope :per_newest, -> (number){page(number).per(PER).order("updated_at DESC")}
   scope :keyword, -> (keyword){ransack(title_cont: keyword).result}
+  scope :lending_books, -> {where(id: Lending.select("book_id").lendings)}
+  scope :safekeeping_books, -> {where.not(id: Lending.select("book_id").lendings)}
 
   class << self
+    # 本一覧
+    # @param          [Hash]   params  パラメータ
+    # @optioin params [String] :page   ページ
+    # @optioin params [String] :status ステータス
+    # @optioin params [String] :q      検索文字列
+    # @return         [Object]         ステータス毎
+    # @return         [Object]         キーワード毎
+    # @return         [Object]         最新ページ毎
     def books(params)
       page = params[:page].to_i ||= 1
-      params[:q] ? keyword(params[:q]).per_newest(page) : per_newest(page)
+      return by_status(page, params[:status].to_i) if params[:status].present?
+      return keyword(params[:q]).per_newest(page) if params[:q].present?
+      return per_newest(page) if params[:q].blank?
     end
+
+    # ステータス毎
+    # @param  [Numeric] page   ページ
+    # @param  [Numeric] status ステータス
+    # @return [Object]         貸出中本一覧
+    # @return [Object]         保管中本一覧
+    private
+      def by_status(page, status)
+        return lending_books.per_newest(page) if status == statuses["lending"]
+        return safekeeping_books.per_newest(page) if status == statuses["safekeeping"]
+      end
   end
 end
