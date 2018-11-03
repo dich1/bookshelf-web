cfcea4ab1953f059c6026d5c9e415feb70026642
大智 伊藤 <daich125912@gmail.com>
2018-11-03 18:03:27 +0000

    [fix]Facebookログインユーザー名の日本語取得

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,7 +258,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET']
+  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET'], locale: 'ja_JP', provider_ignores_state: true
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
