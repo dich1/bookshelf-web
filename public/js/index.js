a2b0aaf42c377b90446c2eba00dfae091e8cff67
大智 伊藤 <daich125912@gmail.com>
2018-11-04 03:49:50 +0000

    [feat]ユーザー切出し時の削除漏れ

--- public/js/index.js
+++ public/js/index.js
@@ -50,46 +50,6 @@ function setBooksCount(readings, safekeepings) {
 }
 
 /**
- * ユーザー情報を取得する
- * @return {Object} ユーザー情報
- */
-function getUser() {
-    var storage = sessionStorage;
-    if (storage.getItem('user_id')) {
-        setUser(storage.getItem('user_name'), storage.getItem('user_image'));
-        return;
-    }
-    var endpointName = 'ユーザー取得API';
-    var getUser = Api.getUser();
-    getUser.done(function(data){
-        console.log(endpointName + '：' + getUser.status);
-        login(data.user_id, data.user_name, data.user_image);
-        displayAlert('ログインしました。');
-    }).fail(function(data, textStatus, errorThrown) {
-        logout();
-        displayAlert('借りるにはログインが必要です。');
-    });    
-}
-
-/**
- * ユーザー情報を設定する
- * @param {Object} user ユーザー情報
- */
-function setUser(userName, userImage) {
-    var nav = document.getElementsByClassName('logout')[0];
-    nav.setAttribute('class', 'login');
-    var userElement = document.getElementById('user');
-    var userMenuElement = '<a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 0px 8px; background-color: #fff0;">'
-                        + '<img src="' + userImage + '" alt="" >' + userName
-                        + '</a>'
-                        + '<span class="dropdown-menu" aria-labelledby="dropdownMenuLink">'
-                        + '<a class="dropdown-item" href="/users/edit">設定変更</a>'
-                        + '<a class="dropdown-item" href="/users/sign_out" onclick="logout();">ログアウト</a>'
-                        + '</span>';
-    userElement.innerHTML = userMenuElement;
-}
-
-/**
  * 本一覧を取得する
  * @param {number} status  本ステータス
  * @param {number} page    ページ
