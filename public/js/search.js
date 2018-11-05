97f114b96809fab2fca266c9211f2be5b647e152
大智 伊藤 <daich125912@gmail.com>
2018-11-05 19:35:46 +0000

    [feat]保存前書籍検索をボタンからするように変更

--- public/js/search.js
+++ public/js/search.js
@@ -2,10 +2,7 @@ $(document).ready(function() {
     $("#form_book_title").keyup(function() {
         var keyword = $(this).val();
         setTimeout(function(){
-            if (location.pathname === '/form.html') {
-                return searchBook(keyword);
-            }
-            return getBooks(null, 1, keyword);
+            getBooks(null, 1, keyword);
         }, 1000);
     });
 });
