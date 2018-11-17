cf8764f6ec269381007754b591be322fb2bb62fb
伊藤 大智 <daichi.ito@ever-rise.co.jp>
2018-11-17 16:35:29 +0900

    [chore]デプロイ先サーバーを変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ set :branch, 'master'
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "54.64.252.121", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "52.192.137.28", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
