d470790c1200db366604121601c4e7ed098d4ce8
Daichi Itoh <daich125912@gmail.com>
2018-12-26 03:00:32 +0000

    [test]テストケースのみの追加

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -10,14 +10,26 @@ RSpec.describe Genre, type: :model do
       expect(@genre.valid?).to eq(true) 
     end
 
+    it "is uniqueness a name" do
+
+    end
+
+    it "is has many books" do
+      # expect(@genre.books).to match_array books
+    end
+
+    it "is not has many books" do
+
+    end
+
     it "is invalid without a name ('')" do
       @genre.name = ''
-      expect(@genre.valid?).to eq(false) 
+      expect(@genre.invalid?).to eq(true) 
     end
 
     it "is invalid without a name (nil)" do
       @genre.name = nil
-      expect(@genre.valid?).to eq(false) 
+      expect(@genre.invalid?).to eq(true) 
     end
   end
 end
