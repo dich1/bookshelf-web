c030ef8d583283d27fa310e02ad3fd20765324fa
Daichi Itoh <daich125912@gmail.com>
2019-01-05 11:45:01 +0000

    [refactor]letへの置替え

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  before do 
-    @genre = Genre.new()
-  end
+  let(:genre) { Genre.new(params) }
+  let(:params) { { name: 'TEST NAME' } }
 
   describe "Genre" do
     it "is valid with a name" do
-      @genre.name = 'TEST NAME'
-      expect(@genre.valid?).to eq(true)
+      expect(genre.valid?).to eq(true)
     end
 
     it "is uniqueness a name" do
       Genre.create(name: 'TEST NAME')
-      @genre.name = 'TEST NAME'
-      expect(@genre.invalid?).to eq(true)
+      expect(genre.invalid?).to eq(true)
     end
 
     it "is has many books" do
@@ -22,29 +19,27 @@ RSpec.describe Genre, type: :model do
       1.upto(2){|number|
         books << Book.create(title: 'title' + number.to_s, image: 'image' + number.to_s)
       }
-      @genre.name = 'TEST NAME'
-      @genre.books = books
-      expect(@genre.books).to match_array books
+      genre.books = books
+      expect(genre.books).to match_array books
     end
 
     it "is not has many books" do
-      @genre.name = 'TEST NAME'
-      expect(@genre.books.size).to eq(0)
+      expect(genre.books.size).to eq(0)
     end
 
     it "is invalid without a name number" do
-      @genre.name = 1
-      expect(@genre.valid?).to eq(true)
+      genre.name = 1
+      expect(genre.valid?).to eq(true)
     end
 
     it "is invalid without a name ('')" do
-      @genre.name = ''
-      expect(@genre.invalid?).to eq(true) 
+      genre.name = ''
+      expect(genre.invalid?).to eq(true)
     end
 
     it "is invalid without a name (nil)" do
-      @genre.name = nil
-      expect(@genre.invalid?).to eq(true) 
+      genre.name = nil
+      expect(genre.invalid?).to eq(true)
     end
   end
 end
